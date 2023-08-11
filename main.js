90514f82d9679bbd0a126ca1c7acdc8236d1c54f
pbroz35 <1brozek.peter@gmail.com>
2023-08-11 11:58:23 -0500

    added operator function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let userInput1
+userInput2
+operator;
+
 
 //addition
 function add(num1, num2){
@@ -9,4 +13,33 @@ function subtract(num1, num2){
     return num1-num2;
 }
 
-console.log(subtract(5,2));
+//multiplication
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+//divide
+function divide(num1, num2){
+    return num1/num2;
+}
+
+//operate function
+
+function operate(userInput1, operator, userInput2){
+    if (operator=='+')
+    {
+        add(userInput1,userInput2);
+    }
+    if(operator=='-')
+    {
+        subtract(userInput1,userInput2);
+    }
+    if(operator=='*'){
+        multiply(userInput1,userInput2);
+    }
+    if(operator=='/')
+    {
+        divide(userInput1,userInput2);
+    }
+
+}
